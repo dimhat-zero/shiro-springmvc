86c8b9ff2963ac0aadfcdf777e31fcfde7e65517
dimhat <dimhat@qq.com>
2015-12-31 01:34:30 +0800

    add new sql file

--- spring-shiro.sql
+++ spring-shiro.sql
@@ -10,38 +10,44 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2015-12-30 17:33:14
+Date: 2015-12-31 01:33:53
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for sys_permissions
+-- Table structure for sys_resource
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_permissions`;
-CREATE TABLE `sys_permissions` (
+DROP TABLE IF EXISTS `sys_resource`;
+CREATE TABLE `sys_resource` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `permission` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `available` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `type` varchar(50) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `parent_ids` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `parent_ids` (`parent_ids`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_permissions
+-- Records of sys_resource
 -- ----------------------------
-INSERT INTO `sys_permissions` VALUES ('1', 'user:add', '增加用户权限', null);
-INSERT INTO `sys_permissions` VALUES ('2', 'user:delete', '删除用户的权限', null);
-INSERT INTO `sys_permissions` VALUES ('3', 'user:update', '修改用户权限', null);
-INSERT INTO `sys_permissions` VALUES ('4', 'user:view', '查看用户的权限', null);
-INSERT INTO `sys_permissions` VALUES ('5', 'user:huiyuan', null, null);
-INSERT INTO `sys_permissions` VALUES ('6', 'user:superhuiyuan', null, null);
+INSERT INTO `sys_resource` VALUES ('1', 'user:add', '增加用户权限', null, null, null, null, null);
+INSERT INTO `sys_resource` VALUES ('2', 'user:delete', '删除用户的权限', null, null, null, null, null);
+INSERT INTO `sys_resource` VALUES ('3', 'user:update', '修改用户权限', null, null, null, null, null);
+INSERT INTO `sys_resource` VALUES ('4', 'user:info', null, null, null, null, null, null);
+INSERT INTO `sys_resource` VALUES ('5', 'user:huiyuan', null, null, null, null, null, null);
+INSERT INTO `sys_resource` VALUES ('6', 'user:superhuiyuan', null, null, null, null, null, null);
 
 -- ----------------------------
--- Table structure for sys_roles
+-- Table structure for sys_role
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_roles`;
-CREATE TABLE `sys_roles` (
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `role` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -50,36 +56,38 @@ CREATE TABLE `sys_roles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_roles
+-- Records of sys_role
 -- ----------------------------
-INSERT INTO `sys_roles` VALUES ('1', 'admin', null, '0');
-INSERT INTO `sys_roles` VALUES ('2', '老板', null, null);
-INSERT INTO `sys_roles` VALUES ('3', '游客', null, null);
-INSERT INTO `sys_roles` VALUES ('4', '普通会员', null, null);
-INSERT INTO `sys_roles` VALUES ('5', '高级会员', null, null);
+INSERT INTO `sys_role` VALUES ('1', 'admin', null, '0');
+INSERT INTO `sys_role` VALUES ('2', '老板', null, null);
+INSERT INTO `sys_role` VALUES ('3', '游客', null, null);
+INSERT INTO `sys_role` VALUES ('4', '普通会员', null, null);
+INSERT INTO `sys_role` VALUES ('5', '高级会员', null, null);
 
 -- ----------------------------
--- Table structure for sys_roles_permissions
+-- Table structure for sys_role_resource
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_roles_permissions`;
-CREATE TABLE `sys_roles_permissions` (
+DROP TABLE IF EXISTS `sys_role_resource`;
+CREATE TABLE `sys_role_resource` (
   `role_id` bigint(20) NOT NULL,
-  `permission_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`role_id`,`permission_id`)
+  `resource_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`resource_id`),
+  KEY `sys_role_resource_ibfk_2` (`resource_id`),
+  CONSTRAINT `sys_role_resource_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`),
+  CONSTRAINT `sys_role_resource_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `sys_resource` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_roles_permissions
+-- Records of sys_role_resource
 -- ----------------------------
-INSERT INTO `sys_roles_permissions` VALUES ('1', '1');
-INSERT INTO `sys_roles_permissions` VALUES ('1', '2');
-INSERT INTO `sys_roles_permissions` VALUES ('1', '3');
-INSERT INTO `sys_roles_permissions` VALUES ('2', '3');
-INSERT INTO `sys_roles_permissions` VALUES ('2', '5');
-INSERT INTO `sys_roles_permissions` VALUES ('3', '5');
-INSERT INTO `sys_roles_permissions` VALUES ('3', '6');
-INSERT INTO `sys_roles_permissions` VALUES ('4', '3');
-INSERT INTO `sys_roles_permissions` VALUES ('4', '4');
+INSERT INTO `sys_role_resource` VALUES ('1', '1');
+INSERT INTO `sys_role_resource` VALUES ('1', '2');
+INSERT INTO `sys_role_resource` VALUES ('1', '3');
+INSERT INTO `sys_role_resource` VALUES ('2', '3');
+INSERT INTO `sys_role_resource` VALUES ('2', '4');
+INSERT INTO `sys_role_resource` VALUES ('2', '5');
+INSERT INTO `sys_role_resource` VALUES ('3', '5');
+INSERT INTO `sys_role_resource` VALUES ('3', '6');
 
 -- ----------------------------
 -- Table structure for sys_url_filter
@@ -91,7 +99,8 @@ CREATE TABLE `sys_url_filter` (
   `url` varchar(100) DEFAULT NULL,
   `roles` varchar(255) DEFAULT NULL,
   `permissions` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `url` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -99,38 +108,42 @@ CREATE TABLE `sys_url_filter` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for sys_users
+-- Table structure for sys_user
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_users`;
-CREATE TABLE `sys_users` (
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) DEFAULT NULL,
   `password` varchar(32) DEFAULT NULL,
   `locked` tinyint(1) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_users
+-- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_users` VALUES ('1', 'admin', '202CB962AC59075B964B07152D234B70', '0', null);
-INSERT INTO `sys_users` VALUES ('2', 'guest', '202CB962AC59075B964B07152D234B70', '0', null);
-INSERT INTO `sys_users` VALUES ('3', 'dimhat', '202CB962AC59075B964B07152D234B70', '0', null);
+INSERT INTO `sys_user` VALUES ('1', 'admin', '202CB962AC59075B964B07152D234B70', '0', null);
+INSERT INTO `sys_user` VALUES ('2', 'guest', '202CB962AC59075B964B07152D234B70', '0', null);
 
 -- ----------------------------
--- Table structure for sys_users_roles
+-- Table structure for sys_user_role
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_users_roles`;
-CREATE TABLE `sys_users_roles` (
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
   `user_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`)
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `sys_user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`),
+  CONSTRAINT `sys_user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_users_roles
+-- Records of sys_user_role
 -- ----------------------------
-INSERT INTO `sys_users_roles` VALUES ('1', '1');
-INSERT INTO `sys_users_roles` VALUES ('1', '3');
-INSERT INTO `sys_users_roles` VALUES ('2', '4');
+INSERT INTO `sys_user_role` VALUES ('1', '1');
+INSERT INTO `sys_user_role` VALUES ('1', '3');
+INSERT INTO `sys_user_role` VALUES ('2', '3');
+INSERT INTO `sys_user_role` VALUES ('1', '4');
